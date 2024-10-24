c0c2c55c0179167d96993154877bd8ecb621bcc3
Igor <kasusplug@gmail.com>
2024-10-24 20:53:00 +0300

    saving what i have done

--- bot_work_parser.py
+++ bot_work_parser.py
@@ -8,10 +8,9 @@ import openpyxl
 load_dotenv(r'C:\Users\kasus\Desktop\pythonapps\calculator_work\data.env')
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-
+user_data = {}
 
 
 class Calculator:
@@ -22,31 +21,23 @@ class Calculator:
         workbook = openpyxl.load_workbook(self.file_name_read)
         sheet = workbook.active
 
-        user_input_pol = input("Please input POL: ").strip().lower()
-        user_input_container = input("Please input container type (20DC/40HQ): ").strip().lower()
-        user_input_dropoff = input("Please input drop off place: ").strip().lower()
-
+        results = []
         for row in sheet.iter_rows(min_row=2, values_only=True): 
-            country, pol, pod, container, railway, freight, unloading, service, shipping, dropoff = row
+            country, pol_row, pod, container_row, railway, freight, unloading, service, shipping, dropoff_row = row
 
-            if pol.strip().lower() == user_input_pol and dropoff.strip().lower() == user_input_dropoff:
-                if user_input_container == container:
-                    if user_input_container == "20dc":
-                        print(f"Match found for 20DC: {{'country': {country}, 'pol': {pol}, 'pod': {pod}, 'container': {container}, \n"
+            if pol_row.strip().lower() == pol and dropoff_row.strip().lower() == dropoff:
+                if container == container_row:
+                    result = (f"Match found for 20DC: {{'country': {country}, 'pol': {pol}, 'pod': {pod}, 'container': {container}, \n"
                               f"'railway rate': {railway}, 'freight price': {freight}, 'unloading price': {unloading}, \n"
                               f"'shipping line': {shipping}, 'dropoff place': {dropoff}}}")
-                    elif user_input_container == "40hq":
-                        print(f"Match found for 40HQ: {{'country': {country}, 'pol': {pol}, 'pod': {pod}, 'container': {container}, \n"
-                              f"'railway rate': {railway}, 'freight price': {freight}, 'unloading price': {unloading}, \n"
-                              f"'shipping line': {shipping}, 'dropoff place': {dropoff}}}")
-    
+                    results.append(result)
+                return results
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(
-        types.InlineKeyboardButton('Тарифы', callback_data="count"),
-        types.InlineKeyboardButton('test', callback_data="test")  
+        types.InlineKeyboardButton('Тарифы', callback_data="count"), 
     )
     bot.send_message(message.chat.id, f"Привет!, {message.from_user.username} \n"
                     "Это бот для расчета тарифов!\n"
@@ -56,6 +47,23 @@ def start_bot(message):
                     "Место сдачи порожнего - только латиницей(например - moscow)\n"
                     "Команды доступные в данном боте -"
                     "/start - команда которая вызывает данное сообщение\n", reply_markup=markup)
+
+
+@bot.callback_query_handler(func= lambda call: call.data == 'count')
+def handle_count(call):
+    user_data[call.from_user.id] = {}
+    bot.send_message(call.message.chat.id, 'Введите порт отправления: ')
+    bot.register_next_step_handler(call.message, get_pol)
+
+def get_pol(message):
+    user_data[message.from_user.id]['pol'] = message.text.strip().lower()
+    bot.send_message(message.chat.id, 'Введите тип контейнера: ')
+    bot.register_next_step_handler(message, get_container)
+
+def get_container(message):
+    user_data[message.from_user.id]['pol'] = message.text.strip().lower()
+    bot.send_message(message.chat.id, 'Введите тип контейнера: ')
+    bot.register_next_step_handler(message, get_container)
 
 
 @bot.message_handler(commands=['generate'])
