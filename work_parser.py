9ccfc6d252353548a6f44343b0501de90e93e3e8
Igor <kasusplug@gmail.com>
2024-10-24 20:14:03 +0300

    small fixes

--- work_parser.py
+++ work_parser.py
@@ -5,7 +5,6 @@ class Calculator:
         self.file_name_read = file_name_read
 
     def parse_excel(self):
-
         workbook = openpyxl.load_workbook(self.file_name_read)
         sheet = workbook.active
 
@@ -14,13 +13,18 @@ class Calculator:
         user_input_dropoff = input("Please input drop off place: ").strip().lower()
 
         for row in sheet.iter_rows(min_row=2, values_only=True): 
-            country, pol, pod, dc20, hq40, railway_20, railway_40, dthc, service, shipping, dropoff, price_20dc, price_40hq = row
+            country, pol, pod, container, railway, freight, unloading, service, shipping, dropoff = row
 
             if pol.strip().lower() == user_input_pol and dropoff.strip().lower() == user_input_dropoff:
-                if user_input_container == "20dc":
-                    print(f"Match found for 20DC: {row}")
-                elif user_input_container == "40hq":
-                    print(f"Match found for 40HQ: {row}")
+                if user_input_container == container:
+                    if user_input_container == "20dc":
+                        print(f"Match found for 20DC: {{'country': {country}, 'pol': {pol}, 'pod': {pod}, 'container': {container}, \n"
+                              f"'railway rate': {railway}, 'freight price': {freight}, 'unloading price': {unloading}, \n"
+                              f"'shipping line': {shipping}, 'dropoff place': {dropoff}}}")
+                    elif user_input_container == "40hq":
+                        print(f"Match found for 40HQ: {{'country': {country}, 'pol': {pol}, 'pod': {pod}, 'container': {container}, \n"
+                              f"'railway rate': {railway}, 'freight price': {freight}, 'unloading price': {unloading}, \n"
+                              f"'shipping line': {shipping}, 'dropoff place': {dropoff}}}")
 
 calc = Calculator(file_name_read='calc_draft.xlsx') 
 calc.parse_excel()
